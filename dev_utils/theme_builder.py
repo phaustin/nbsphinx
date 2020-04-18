964c4dce7af00a4091e776bb1663528b14fe0721
Matthias Geier <Matthias.Geier@gmail.com>
2020-04-18 14:39:06 +0200

    Add argument parsing for theme names

--- dev_utils/theme_builder.py
+++ dev_utils/theme_builder.py
@@ -1,12 +1,25 @@
+#!/usr/bin/env python3
+"""Build multiple versions of the docs with different themes.
+
+If no THEME-NAME is given, all theme branches are built.
+
+"""
 import argparse
 from pathlib import Path
 
 import git
 from sphinx.cmd.build import build_main
 
-main_dir = Path(__file__).resolve().parent
-worktree_dir = main_dir / '_worktree'
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+parser.add_argument(
+    'themes', metavar='THEME-NAME', nargs='*',
+    help='theme name (according to "*-theme" branch name)')
+args = parser.parse_args()
 
+main_dir = Path(__file__).resolve().parent
 repo = git.Repo(main_dir, search_parent_directories=True)
 
 for remote in repo.remotes:
@@ -17,11 +30,21 @@ else:
     raise ValueError('theme_remote not found')
 
 themes = []
-for remote_branch in theme_remote.refs:
-    if remote_branch.remote_head.endswith('-theme'):
-        themes.append(
-            (remote_branch.remote_head[:-len('-theme')], remote_branch.name))
+if args.themes:
+    for remote_branch in theme_remote.refs:
+        theme_name = remote_branch.remote_head[:-len('-theme')]
+        if theme_name in args.themes:
+            themes.append((theme_name, remote_branch.name))
+            args.themes.remove(theme_name)
+    if args.themes:
+        parser.exit('theme(s) not found: {}'.format(args.themes))
+else:
+    for remote_branch in theme_remote.refs:
+        if remote_branch.remote_head.endswith('-theme'):
+            themes.append(
+                (remote_branch.remote_head[:-len('-theme')], remote_branch.name))
 
+worktree_dir = main_dir / '_worktree'
 if not worktree_dir.exists():
     repo.git.worktree('prune')
     repo.git.worktree('add', worktree_dir, '--detach')
